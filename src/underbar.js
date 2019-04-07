e5208cd6ef93bc404778f0ae931ff3ed0fce292d
Abdulrohman-Alakrad <Abdulrohman20153091@gmail.com>
2019-04-07 11:43:07 +0300

    UNDER

--- src/underbar.js
+++ src/underbar.js
@@ -38,7 +38,7 @@
   // Like first, but for the last elements. If n is undefined, return just the
   // last element.
   _.last = function(array, n) {
-    return n === undefined ? array[array.length-1] : array.slice(0, n);
+    return n === undefined ? array[array.length - 1] : array.slice(0, n);
   };
 
   // Call iterator(value, key, collection) for each element of collection.
@@ -47,15 +47,15 @@
   // Note: _.each does not have a return value, but rather simply runs the
   // iterator function over each item in the input collection.
   _.each = function(collection, iterator) {
-   if(Array.isArray(collection)){
-    for(var i = 0; i < collection.length ; i++ ){
-      iterator(collection[i],i,collection);
+    if(Array.isArray(collection)){
+      for(var i = 0; i < collection.length ; i++ ){
+        iterator(collection[i], i, collection);
+      }
+    } else {
+      for (var key in collection){
+        iterator(collection[key], key, collection);
+      }
     }
-  } else {
-    for (var key in collection){
-      iterator(collection[key],key,collection);
-    }
-  }
 
      
   };
@@ -71,7 +71,7 @@
     _.each(array, function(item, index) {
       if (item === target && result === -1) {
         result = index;
-        
+
       }
     });
 
